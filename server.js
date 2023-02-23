ec8550fe83b6e365e89cf967b3ce18335924aa52
gz244 <gz244@cornell.edu>
2023-02-22 22:08:19 -0500

    finsihed set up mongodb as db

---
+++ server.js
@@ -0,0 +1,23 @@
+// connect to config.env
+require('dotenv').config({path: './config.env'});
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// connect to database
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true}, err =>{
+    if (err) throw err;
+    console.log(`Connected to Mongodb!`)
+});
+const port = process.env.PORT || 4242;
+
+app.listen(port, () => { 
+    console.log(`Server is running on port ${port}`)
+});
